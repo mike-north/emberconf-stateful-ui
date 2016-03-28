3e6b21ce4dc1c00f9cde84da3eebe7ce886c9d6d
Mike North <michael.l.north@gmail.com>
2016-03-28 10:31:30 -0700

    complete example

--- app/components/compose-pr-comment.js
+++ app/components/compose-pr-comment.js
@@ -1,27 +1,27 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { inject } = Ember;
 
 export default Ember.Component.extend({
   store: inject.service(),
   classNames: ['compose-pr-comment'],
-  init() {
-    this._super(...arguments);
-    this.set('model', this.get('store').createRecord('comment', {
-      pull: this.get('pr'),
-      body: 'abc',
-      path: '',
-      diffHunk: '',
-      commitId: ''
-    }));
-    debugger;
-  },
+  saving: false,
+  model: stateFor('draft-pull-comment', 'pr'),
   actions: {
     cancel() {
-
+      this.set('model.body', '');
     },
     submit(){
-      this.get('model').save();
+      const model = this.get('model');
+      const attributes = Ember.$.extend(model, {
+        pull: this.get('pr')
+      });
+      this.set('saving', true);
+      this.get('store').createRecord('comment', attributes).save().then(() =>{
+        this.set('saving', false);
+        this.set('model.body', '');
+      });
     }
   }
 });
--- app/helpers/markdown.js
+++ app/helpers/markdown.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export function markdown(params/*, hash*/) {
   const [content] = params;
-  const html = new showdown.Converter().makeHtml(content);
-  return new Ember.Handlebars.SafeString(html);
+  return new showdown.Converter().makeHtml(content);
 }
 
 export default Ember.Helper.helper(markdown);
