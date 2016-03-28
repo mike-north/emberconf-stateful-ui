379ab7d5b5441a95f5095c77f06f8d12dc29b0d4
Mike North <michael.l.north@gmail.com>
2016-03-28 12:23:32 -0700

    guards

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   host: 'https://emberconf-state-api.herokuapp.com',
+  // host: 'http://localhost:4000',
   namespace: 'api',
   coalesceFindRequests: true,
   ajaxOptions() {
--- app/components/compose-pr-comment.js
+++ app/components/compose-pr-comment.js
@@ -8,9 +8,15 @@ export default Ember.Component.extend({
   classNames: ['compose-pr-comment'],
   saving: false,
   model: stateFor('draft-pull-comment', 'pr'),
+  init(){
+    this._super(...arguments);
+    this.set('errors', []);
+  },
   actions: {
     cancel() {
       this.set('model.body', '');
+      this.set('saving', false);
+      this.set('errors', []);
     },
     submit(){
       const body = this.get('model.body');
@@ -20,8 +26,16 @@ export default Ember.Component.extend({
       };
       this.set('saving', true);
       this.get('store').createRecord('comment', attributes).save().then(() =>{
-        this.set('saving', false);
-        this.set('model.body', '');
+        if (!this.isDestroyed && !this.isDestroying) {
+          this.set('saving', false);
+          this.set('errors', []);
+          this.set('model.body', '');
+        }
+      }).catch((err) => {
+        if (!this.isDestroyed && !this.isDestroying) {
+          this.set('errors', err.errors);
+          this.set('saving', false);
+        }
       });
     }
   }
