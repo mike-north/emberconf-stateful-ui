258bf399a5d0b1cc63339140d93855d28f82930b
Mike North <michael.l.north@gmail.com>
2016-03-28 16:44:51 -0700

    guard against component being destroyed

--- app/components/compose-pr-comment.js
+++ app/components/compose-pr-comment.js
@@ -6,17 +6,36 @@ export default Component.extend({
   classNames: ['compose-pr-comment'],
   store: inject.service(),
   body: '',
+  loading: false,
+  error: '',
   actions: {
     save() {
+      this.set('loading', true);
       this.get('store').createRecord('comment', {
         pull: this.get('pr'),
         body: this.get('body')
       }).save().then(() => {
-        this.set('body', '');
+        if (!(this.isDestroyed || this.isDestroying)) {
+          this.set('body', '');
+        }
+      }).catch((errInfo) => {
+        let errString = errInfo.errors
+          .map((err) => err.detail)
+          .join(', ');
+        if (!(this.isDestroyed || this.isDestroying)) {
+          this.set('error', errString);
+        }
+      }).finally(() => {
+        if (!(this.isDestroyed || this.isDestroying)) {
+          this.set('loading', false);
+        }
       });
     },
     cancel() {
-      this.set('body', '');
+      if (!(this.isDestroyed || this.isDestroying)) {
+        this.set('body', '');
+        this.set('loading', false);
+      }
     }
   }
 });
