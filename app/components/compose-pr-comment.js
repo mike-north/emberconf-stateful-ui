7a2a26f6cdc5ea52da6ee5d20815cee0910d9ac5
Mike North <michael.l.north@gmail.com>
2016-03-28 16:10:17 -0700

    Compose a new PR comment

---
+++ app/components/compose-pr-comment.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+const { Component, inject } = Ember;
+
+export default Component.extend({
+  classNames: ['compose-pr-comment'],
+  store: inject.service(),
+  body: '',
+  actions: {
+    save() {
+      this.get('store').createRecord('comment', {
+        pull: this.get('pr'),
+        body: this.get('body')
+      }).save().then(() => {
+        this.set('body', '');
+      });
+    },
+    cancel() {
+
+    }
+  }
+});
