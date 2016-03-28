a3e1569b174c9bddc1ee18c09c72fe94c07daccf
Mike North <michael.l.north@gmail.com>
2016-03-28 15:21:57 -0700

    exercise 2 - Navigational state

--- app/routes/organization/repository/pulls.js
+++ app/routes/organization/repository/pulls.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  queryParams: {
+    state: {
+      refreshModel: true
+    }
+  },
   model(params /*, transition*/) {
     let repo = this.modelFor('organization.repository');
-    return repo.get('pulls');
+    let args = {
+      repository_id: repo.get('id')
+    };
+    if (params.state) {
+      args.state = params.state;
+    }
+    return this.store.query('pull', args);
   }
 });
